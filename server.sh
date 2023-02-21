027eecd4f5f7a9a72f755bb80448e1739da9b187
abao <zhangbaocai@soimit.com>
2023-02-21 16:06:53 +0800

    Update server.sh for init.log of stderr.

--- server.sh
+++ server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python -m uiautomator2 init 2>&1 >> init.log
+python -m uiautomator2 init >> init.log 2>&1
 
 daemon -rU --name weditor --chdir=$PWD --stdout $PWD/stdout.log --stderr $PWD/stderr.log -- python -m weditor -q
