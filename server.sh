d7714b8ba683860b0f58ab09ff5d7048bb80d8a4
abao <talent518@yeah.net>
2023-12-24 23:41:57 +0800

    Fix server.sh script of bug.

--- server.sh
+++ server.sh
@@ -3,7 +3,7 @@
 echo "Ignore sound device found: touch .ignore.pcm"
 
 adb tcpip 5555 >> init.log 2>&1
-sleep 2
+sleep 10
 adb root >> init.log 2>&1
 sleep 2
 adb shell setprop bmi.service.adb.root 1 >> init.log 2>&1
