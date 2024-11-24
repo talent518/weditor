d8f1424f48562c87d487b3a80b96bd4fd261227a
abao <talent518@yeah.net>
2024-11-24 14:00:40 +0800

    Optimize for server.sh

--- server.sh
+++ server.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 echo "Ignore sound device found: touch .ignore.pcm"
+echo "Ignore adb and uiautomator2 init: touch .ignore.init"
+echo "weditor argument file: .weditor.arg"
 
-adb tcpip 5555 >> init.log 2>&1
-sleep 10
-adb root >> init.log 2>&1
-sleep 2
-adb shell setprop bmi.service.adb.root 1 >> init.log 2>&1
-adb shell "setprop bmi.service.adb.root 1" >> init.log 2>&1
-python -m uiautomator2 purge >> init.log 2>&1
-python -m uiautomator2 init >> init.log 2>&1
-adb forward --list >> init.log 2>&1
+truncate -s0 *.log
+
+if [ ! -f ".ignore.init" ]; then
+    adb tcpip 5555 >> init.log 2>&1
+    sleep 10
+    adb root >> init.log 2>&1
+    sleep 2
+    adb shell setprop bmi.service.adb.root 1 >> init.log 2>&1
+    adb shell "setprop bmi.service.adb.root 1" >> init.log 2>&1
+    python -m uiautomator2 purge >> init.log 2>&1
+    python -m uiautomator2 init >> init.log 2>&1
+    adb forward --list >> init.log 2>&1
+fi
 
 arg="-q"
 
@@ -19,6 +25,10 @@ if [ ! -f ".ignore.pcm" ]; then
     if [ -n "$d" ]; then
         arg="$arg -d $d"
     fi
+fi
+
+if [ -f ".weditor.arg" ]; then
+    arg="$arg $(cat .weditor.arg)"
 fi
 
 daemon --stop --name weditor
@@ -30,6 +40,8 @@ if [ -f "$PIDFILE" ]; then
         kill $PID
         while [ -d "/proc/$PID" ]; do sleep 1;done
     fi
-    rm -vf "$PIDFILE"
+    rm -f "$PIDFILE"
 fi
-daemon -irU -M 1000000 -L 10 --name weditor --chdir=$PWD -- sh -c "python -m weditor $arg < /dev/null >> stdout.log 2>> stderr.log"
+
+daemon -irU -M 1000000 -L 10 --name weditor --chdir=$PWD --stdout stdout.log --stderr stderr.log -- python -m weditor $arg $@
+
